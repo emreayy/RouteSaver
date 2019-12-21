7b5ea783c734c9c18bed1ad042d4fe7feb1c1ed0
emreayy <emre.ayy@yandex.com>
2019-12-21 17:57:20 +0300

    Print coordinates for testing

--- screens/RouteStart.js
+++ screens/RouteStart.js
@@ -6,11 +6,11 @@ export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isStarted: false
+      isStarted: false,
+      list: []
     }
 
     this.intervalId = -1
-    this.list = []
 
   }
 
@@ -18,7 +18,13 @@ export default class HomeScreen extends Component {
     if (this.state.isStarted) return;
 
     this.setState({isStarted: true})
-    let date = new Date()
+    this.setState(prevState => {
+      let list = prevState.list
+      list.push([])
+      return {
+        list
+      }
+    })
     this.intervalId = setInterval(this.getLocation, freq)
   }
 
@@ -31,7 +37,29 @@ export default class HomeScreen extends Component {
 
   getLocation = () => {
     let random = Math.random() * 10000
-    Alert.alert("Selamlar" + random)
+    //Alert.alert("Selamlar" + random)
+
+    this.findCoordinates()
+  }
+
+  findCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const {latitude, longitude} = position.coords
+        const lastIndex = this.state.list.length - 1
+        let date = new Date()
+
+        this.setState(prevState => {
+          let list = prevState.list
+          list[lastIndex].push({latitude, longitude, date})
+          return {
+            list
+          }
+        })
+      },
+      error => Alert.alert(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    )
   }
 
   render() {
@@ -39,6 +67,19 @@ export default class HomeScreen extends Component {
       <View>
       <Button title="Start" onPress={this.handleStart}/>
       <Button title="Stop" onPress={this.handleStop}/>
+      <Text>Adet sayisi:{this.state.list.length && this.state.list[0].length}</Text>
+      {
+        this.state.list.map((eleman, key) => {
+            
+          if (eleman.length <= 0) return <Text key={key}>Boş eleman</Text>;
+
+          return eleman.map((el, key) => {
+            return <Text key={key}>{el.latitude + " - " + el.longitude + " - " + el.date.getSeconds()}</Text>
+          })
+
+          
+        })
+      }
       </View>
     );
   }
